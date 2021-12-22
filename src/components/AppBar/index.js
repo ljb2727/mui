8e92ba0bc07d4d91bc52d098203499b82b732730
ljb2727 <ljb2727@gmail.com>
2021-12-22 13:22:49 +0900

    prettier 설정

--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -2,106 +2,101 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-
-import { BrowserRouter as Router ,Routes, Route, Link as RouterLink  } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link as RouterLink,
+} from 'react-router-dom';
 
 export default function ColorTabs() {
-    const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
-    console.log("change")
+    console.log('change');
   };
 
+  const Home = () => {
+    return <div>home</div>;
+  };
+  const Booking = () => {
+    return <div>Booking</div>;
+  };
+  const Onebooking = () => {
+    return <div>onebooking</div>;
+  };
 
-
-
-  const menuList = [{
-          id: 1,
-          name: "홈",
-          link: "/home"
-      },
-      {
-          id: 2,
-          name: "부킹",
-          link: "/booking"
-      },
-      {
-          id: 3,
-          name: "1인부킹",
-          link: "/onebooking"
-      }, {
-          id: 4,
-          name: "조인/양도",
-          link: "/transfer"
-      },
-      {
-          id: 5,
-          name: "국내투어",
-          link: "/package"
-      },
-      {
-          id: 6,
-          name: "이용후기",
-          link: "/epilogue"
-      },
-      {
-          id: 7,
-          name: "이벤트",
-          link: "/event"
-      },
-      {
-          id: 8,
-          name: "쇼핑몰",
-          link: "/shop"
-      }
+  const menuList = [
+    {
+      id: 1,
+      name: '홈',
+      link: '/home',
+    },
+    {
+      id: 2,
+      name: '부킹',
+      link: '/booking',
+    },
+    {
+      id: 3,
+      name: '1인부킹',
+      link: '/onebooking',
+    },
+    {
+      id: 4,
+      name: '조인/양도',
+      link: '/transfer',
+    },
+    {
+      id: 5,
+      name: '국내투어',
+      link: '/package',
+    },
+    {
+      id: 6,
+      name: '이용후기',
+      link: '/epilogue',
+    },
+    {
+      id: 7,
+      name: '이벤트',
+      link: '/event',
+    },
+    {
+      id: 8,
+      name: '쇼핑몰',
+      link: '/shop',
+    },
   ];
 
-  const Home =() =>{
-      return (
-          <div>home</div>
-      )
-  }
-  const Booking =() =>{
-    return (
-        <div>Booking</div>
-    )
-}
-const Onebooking =() =>{
-    return (
-        <div>onebooking</div>
-    )
-}
   return (
-     <>
-        <Router>
-            <Box sx={{ minWidth: "100%", bgcolor: 'background.paper' }}>
-                <Tabs
-                value={value}
-                onChange={handleChange}
-                variant="scrollable"
-                scrollButtons="auto"
-                >
-                {menuList.map((item)=>(
-                    <Tab component={RouterLink} label={item.name} to={item.link} key={item.id} />
-                ))}
-                    
-                </Tabs>
-            </Box>
+    <>
+      <Router>
+        <Box sx={{ minWidth: '100%', bgcolor: 'background.paper' }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons="auto"
+          >
+            {menuList.map((item) => (
+              <Tab
+                component={RouterLink}
+                label={item.name}
+                to={item.link}
+                key={item.id}
+              />
+            ))}
+          </Tabs>
+        </Box>
 
-            <Routes>
-              <Route path="/home" exact element={<Home/>}/>
-              <Route path="/booking" element={<Booking/>}/>
-              <Route path="/onebooking" element={<Onebooking/>}/>
-            </Routes>
-            
-        </Router>
-        
-        
-    </>   
-
+        <Routes>
+          <Route path="/home" exact element={<Home />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/onebooking" element={<Onebooking />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
-
