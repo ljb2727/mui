645411d219c010f967d062d0336658a6cc92031e
ljb2727 <ljb2727@gmail.com>
2021-12-22 09:27:24 +0900

    nav

--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 
 export default function ColorTabs() {
     const [value, setValue] = React.useState(0);
@@ -11,6 +12,19 @@ export default function ColorTabs() {
     setValue(newValue);
     console.log("change")
   };
+
+  const LinkTab = (props) => {
+      return(
+          <Tab
+              component="a"
+              sx={{minWidth:"2em"}}
+              onClick={(event) => {
+                    event.preventDefault();
+                }}
+                {...props}
+          />
+      )
+  }
 
   const menuList = [{
           id: 1,
@@ -53,18 +67,21 @@ export default function ColorTabs() {
       }
   ]
   return (
-    <Box sx={{ maxWidth: "100%", bgcolor: 'background.paper' }}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="scrollable"
-        scrollButtons="auto"
-        aria-label="scrollable auto tabs example"
-      >
-        {menuList.map((item)=>(
-            <Tab label={item.name} key={item.id} />
-        ))}
-      </Tabs>
-    </Box>
+    <Router>
+        <Box sx={{ minWidth: "100%", bgcolor: 'background.paper' }}>
+            <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons="auto"
+            aria-label="scrollable auto tabs example"
+            >
+            {menuList.map((item)=>(
+                <LinkTab label={item.name} to={item.link} key={item.id} />
+            ))}
+            </Tabs>
+        </Box>
+    </Router>
   );
 }
+
