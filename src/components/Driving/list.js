20de2d612c6cc2d324a9d9579040e780caaf64b9
ljb2727 <ljb2727@gmail.com>
2021-12-24 17:17:49 +0900

    슬라이더 여러줄

--- src/components/Driving/list.js
+++ src/components/Driving/list.js
@@ -30,29 +30,29 @@ const timeList2 = [
   {
     floor: 2,
     target: [
-      { number: 1, time: 30, option: true },
-      { number: 2, time: 0, option: true },
-      { number: 3, time: 50, option: false },
-      { number: 4, time: 0, option: true },
-      { number: 5, time: 0, option: false },
-      { number: 6, time: 120, option: false },
-      { number: 7, time: 0, option: true },
-      { number: 8, time: 0, option: false },
-      { number: 9, time: 120, option: false },
+      { number: 11, time: 30, option: true },
+      { number: 12, time: 0, option: true },
+      { number: 13, time: 50, option: false },
+      { number: 14, time: 0, option: true },
+      { number: 15, time: 0, option: false },
+      { number: 16, time: 120, option: false },
+      { number: 17, time: 0, option: true },
+      { number: 18, time: 0, option: false },
+      { number: 19, time: 120, option: false },
     ],
   },
   {
     floor: 3,
     target: [
-      { number: 1, time: 30, option: true },
-      { number: 2, time: 0, option: true },
-      { number: 3, time: 50, option: false },
-      { number: 4, time: 0, option: true },
-      { number: 5, time: 0, option: false },
-      { number: 6, time: 120, option: false },
-      { number: 7, time: 0, option: true },
-      { number: 8, time: 0, option: false },
-      { number: 9, time: 120, option: false },
+      { number: 21, time: 30, option: true },
+      { number: 22, time: 0, option: true },
+      { number: 23, time: 50, option: false },
+      { number: 24, time: 0, option: true },
+      { number: 25, time: 0, option: false },
+      { number: 26, time: 120, option: false },
+      { number: 27, time: 0, option: true },
+      { number: 28, time: 0, option: false },
+      { number: 29, time: 120, option: false },
     ],
   },
 ];
@@ -60,8 +60,9 @@ const timeList2 = [
 const DrivingList = () => {
   return (
     <>
-      <BaseSwiper item={timeList} />
-      <BaseSwiper item={timeList2} />
+      {timeList2.map((e, i) => (
+        <BaseSwiper props={timeList2[i]} key={i} />
+      ))}
     </>
   );
 };
--- src/components/Swiper/index.js
+++ src/components/Swiper/index.js
@@ -3,6 +3,7 @@ import 'swiper/css';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/system';
 import theme from 'theme';
+import Divider from '@mui/material/Divider';
 
 import Chip from '@mui/material/Chip';
 
@@ -24,43 +25,46 @@ const StyledChip = styled(Chip)({
   right: 0,
 });
 
-const BaseSwiper = ({ item }) => {
-  console.log(item);
+const BaseSwiper = ({ props }) => {
+  console.log(props.target);
   return (
-    <Swiper
-      spaceBetween={10}
-      slidesPerView={'auto'}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-      style={{ textAlign: 'center' }}
-    >
-      {item.map((item, i) => (
-        <SwiperSlide style={swiperStyle} key={i} value={item.time}>
-          <Box
-            style={{
-              background: item.time ? theme.default : theme.disabled,
-              padding: '10px 4px 4px',
-              borderTopLeftRadius: '10px',
-              borderTopRightRadius: '10px',
-              marginTop: 15,
-            }}
-          >
-            {item.option && (
-              <StyledChip label="좌우 전용" color="info" size="small" />
-            )}
-
-            <CustomBox>{item.number}</CustomBox>
-            <Box mt={1}>
-              {Number(item.time) ? (
-                <Box>{item.time}</Box>
-              ) : (
-                <Box>타임 없음</Box>
+    <>
+      <Divider>{`${props.floor}층`}</Divider>
+      <Swiper
+        spaceBetween={10}
+        slidesPerView={'auto'}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        style={{ textAlign: 'center', marginBottom: 40 }}
+      >
+        {props.target.map((item, i) => (
+          <SwiperSlide style={swiperStyle} key={i} value={item.time}>
+            <Box
+              style={{
+                background: item.time ? theme.default : theme.disabled,
+                padding: '10px 4px 4px',
+                borderTopLeftRadius: '10px',
+                borderTopRightRadius: '10px',
+                marginTop: 15,
+              }}
+            >
+              {item.option && (
+                <StyledChip label="좌우 전용" color="info" size="small" />
               )}
+
+              <CustomBox>{item.number}</CustomBox>
+              <Box mt={1}>
+                {Number(item.time) ? (
+                  <Box>{item.time}</Box>
+                ) : (
+                  <Box>타임 없음</Box>
+                )}
+              </Box>
             </Box>
-          </Box>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
 
